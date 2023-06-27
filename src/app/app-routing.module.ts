8ee864e9e1fef652f69769e3b29d02325c804c46
Jezebel1990 <jezebelguedes@gmail.com>
2023-06-26 22:54:31 -0300

    add route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './postList/home/home.component';
-
+import { PostListComponent } from './postList/components/post-list.component';
 
 
 
 const routes: Routes = [
-    { path: '', component: HomeComponent , pathMatch: 'full'  }, // Redireciona para a rota '/home'
- 
+    { path: '', redirectTo:'home' , pathMatch: 'full'  },
+    {path: 'home',component: HomeComponent},
+    {path: 'post-list', component: PostListComponent},
     //  {path:'', redirectTo:'blogangular', component: PostListComponent}
     // { path: 'contact', component: ContactComponent },
     // { path: '**', component: PageNotFoundComponent } // Rota curinga para lidar com rotas inválidas
--- src/app/dist/app-routing.module.js
+++ src/app/dist/app-routing.module.js
@@ -10,8 +10,11 @@ exports.AppRoutingModule = void 0;
 var core_1 = require("@angular/core");
 var router_1 = require("@angular/router");
 var home_component_1 = require("./postList/home/home.component");
+var post_list_component_1 = require("./postList/components/post-list.component");
 var routes = [
-    { path: '', component: home_component_1.HomeComponent, pathMatch: 'full' },
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: home_component_1.HomeComponent },
+    { path: 'post-list', component: post_list_component_1.PostListComponent },
 ];
 var AppRoutingModule = /** @class */ (function () {
     function AppRoutingModule() {
